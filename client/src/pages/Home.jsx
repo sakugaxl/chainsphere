0d3d5c9ae916816db1eb1af604cee5671cb73807
unknown <bekiwe01@gmail.com>
2023-03-19 23:30:21 +0200

    Homepage/Page bases

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { Loader } from '../components';
 
 const Home = () => {
+
+  const [loading, setLoading] = useState(false);
   return (
-    <div>Home</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Welcome to Chainsphere</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Securing voting systems globally</p>
+      </div>
+      {/* add divs for following page portions */}
+    </section >
   )
 }
 
