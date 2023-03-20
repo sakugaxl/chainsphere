b1d492fd736fac99c9071d046646821545cd179f
unknown <bekiwe01@gmail.com>
2023-03-20 02:37:31 +0200

    Custom CSS added (Tailwind Mainly), Vote System Updated

--- client/src/components/VoteBox.jsx
+++ client/src/components/VoteBox.jsx
@@ -1,47 +1,76 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const VoteOption = ({ value, label, onSubmit }) => {
+  const navigate = useNavigate();
+  const imageURL = 'https://example.com/placeholder.jpg'; // Update this with the actual external URL
+
+  const handleClick = () => {
+    onSubmit(value);
+  };
+
+  const handleReadMore = () => {
+    navigate(`/details/${value}`); // Update this with the correct route for your details page
+  };
+
+  return (
+    <div className="bg-white p-4 rounded-md shadow-md w-64 mx-2 text-white blue-glassmorphism dark-eth-card">
+      <h2 className="text-2xl mb-4">{label}</h2>
+      <div className="mb-4">
+        <img
+          src={imageURL}
+          alt={`Placeholder for ${label}`}
+          className="w-full h-64 object-cover object-center"
+        />
+      </div>
+      <p className="mb-4">
+        This is a description for {label}. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.
+      </p>
+      <button
+        type="button"
+        onClick={handleReadMore}
+        className="mb-4 bg-green-500 hover:bg-green-600 text-white py-1 px-4 rounded-md"
+      >
+        Read More
+      </button>
+      <button
+        type="button"
+        onClick={handleClick}
+        className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+      >
+        Vote
+      </button>
+    </div>
+  );
+};
 
 const VoteBox = () => {
   const [vote, setVote] = useState('');
 
-  const handleVoteChange = (e) => {
-    setVote(e.target.value);
+  const handleSubmit = (option) => {
+    setVote(option);
+    console.log('Vote submitted:', option);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Do something with the vote value, e.g., send it to the server
-    console.log('Vote submitted:', vote);
-    setVote('');
-  };
+  const options = [
+    { value: 'option1', label: 'Option 1' },
+    { value: 'option2', label: 'Option 2' },
+    { value: 'option3', label: 'Option 3' },
+    { value: 'option4', label: 'Option 4' },
+    { value: 'option5', label: 'Option 5' },
+  ];
 
   return (
-    <div className="bg-white p-4 rounded-md shadow-md w-64">
-      <h2 className="text-2xl mb-4">Cast Your Vote</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="vote" className="block mb-1">
-            Choose an option:
-          </label>
-          <select
-            id="vote"
-            name="vote"
-            value={vote}
-            onChange={handleVoteChange}
-            className="w-full px-2 py-1 border border-gray-300 rounded-md"
-          >
-            <option value="">Select an option</option>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-          </select>
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
-        >
-          Submit Vote
-        </button>
-      </form>
+    <div className="flex items-center justify-center">
+      {options.map((option) => (
+        <VoteOption
+          key={option.value}
+          value={option.value}
+          label={option.label}
+          onSubmit={handleSubmit}
+        />
+      ))}
     </div>
   );
 };
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,8 @@
 import Home from './Home'
 import Vote from './Vote'
-
+import Results from './Results'
 export {
     Home,
     Vote,
+    Results,
 }
